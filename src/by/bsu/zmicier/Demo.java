11736625e6ee2d3cbc40ff5a7b864e9caea73509
Zmicier Kachkou <dmitriydikanskiy@gmail.com>
2021-02-14 00:56:40 +0300

    Add Registry pattern

--- src/by/bsu/zmicier/Demo.java
+++ src/by/bsu/zmicier/Demo.java
@@ -1,28 +1,19 @@
 package by.bsu.zmicier;
 
-import by.bsu.zmicier.actions.DoNothingAction;
 import by.bsu.zmicier.actions.DuplicateStringAction;
 import by.bsu.zmicier.actions.ReverseStringAction;
-import by.bsu.zmicier.actions.StringAction;
 import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) {
+        Registry.getInstance().register("duplicate", new DuplicateStringAction());
+        Registry.getInstance().register("reverse", new ReverseStringAction());
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
             String command = scanner.nextLine();
 
-            StringAction action;
-            if ("duplicate".equals(command)) {
-                action = new DuplicateStringAction();
-            } else if ("reverse".equals(command)) {
-                action = new ReverseStringAction();
-            } else {
-                action = new DoNothingAction();
-            }
-
-            System.out.println(action.doAction(input));
+            System.out.println(Registry.getInstance().getAction(command).doAction(input));
         }
     }
 }
--- src/by/bsu/zmicier/Registry.java
+++ src/by/bsu/zmicier/Registry.java
@@ -1,11 +1,33 @@
 package by.bsu.zmicier;
 
-public class Registry {
-    private static final Registry INSTANCE = new Registry();
+import by.bsu.zmicier.actions.DoNothingAction;
+import by.bsu.zmicier.actions.StringAction;
+import java.util.HashMap;
+import java.util.Map;
 
-    private Registry() { }
+public class Registry {
+    private static final Registry INSTANCE = new Registry(new DoNothingAction());
+
+    private final Map<String, StringAction> actions = new HashMap<>();
+    private final StringAction defaultAction;
+
+    private Registry(StringAction action) {
+        this.defaultAction = action;
+    }
 
     public static Registry getInstance() {
         return INSTANCE;
+    }
+
+    public StringAction getAction(String key) {
+        StringAction action = actions.get(key);
+        if (action == null) {
+            action = defaultAction;
+        }
+        return action;
+    }
+
+    public void register(String key, StringAction action) {
+        actions.put(key, action);
     }
 }
